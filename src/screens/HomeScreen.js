2e285774f63e576b5c1286f50589d71b4c741e51
Ömer Faruk Gündoğdu <omerrfarukgundogdu@gmail.com>
2024-10-20 14:29:44 +0300

    Home ekranının tasarımı tamamlandı.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,57 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect, useRef } from "react";
+
+import Lottie from "lottie-react-native";
+
+const { width, height } = Dimensions.get("window");
 
 export default function HomeScreen() {
+  const animationRef = useRef(null);
+
+  useEffect(() => {
+    animationRef.current?.play();
+  }, []);
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.lottie}>
+        <Lottie
+          ref={animationRef}
+          source={require("../assets/animation/confetti.json")}
+          loop
+          style={styles.lottie}
+        />
+      </View>
+      <Text style={styles.text}>Home Page</Text>
+      <TouchableOpacity style={styles.resetButton}>
+        <Text>Reset</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  lottie: {
+    width: width * 0.9,
+    height: width,
+  },
+  text: {
+    fontSize: width * 0.09,
+    marginBottom: 20,
+  },
+  resetButton: {
+    backgroundColor: "#34d399",
+    padding: 10,
+    borderRadius: 10,
+  },
+});
